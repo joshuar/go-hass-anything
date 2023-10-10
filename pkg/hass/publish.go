126f753fb5ed649986de58e88af569226a29576a
Joshua Rich <joshua.rich@gmail.com>
2023-10-10 08:20:31 +0000

    feat(hass): add Register and UnRegister functions
    - Register makes it easy for an app to register its entities and also record
    registration in its config.
    - UnRegister makes it easy for an app to remove its entities.

--- pkg/hass/publish.go
+++ pkg/hass/publish.go
@@ -5,9 +5,13 @@
 
 package hass
 
-import "github.com/joshuar/go-hass-anything/pkg/mqtt"
+import (
+	"github.com/joshuar/go-hass-anything/pkg/config"
+	"github.com/joshuar/go-hass-anything/pkg/mqtt"
+)
 
 type MQTTDevice interface {
+	Name() string
 	Configuration() []*mqtt.MQTTMsg
 	States() []*mqtt.MQTTMsg
 	Subscriptions() []*mqtt.MQTTSubscription
@@ -16,6 +20,39 @@ type MQTTDevice interface {
 type MQTTClient interface {
 	Publish(...*mqtt.MQTTMsg) error
 	Subscribe(...*mqtt.MQTTSubscription) error
+}
+
+func Register(device MQTTDevice, client MQTTClient) error {
+	var cfg config.AppConfig
+	var err error
+	if cfg, err = config.LoadConfig(device.Name()); err != nil {
+		return err
+	}
+	if !cfg.IsRegistered(device.Name()) {
+		if err := PublishConfigs(device, client); err != nil {
+			return err
+		} else {
+			if err = cfg.Register(device.Name()); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func UnRegister(device MQTTDevice, client MQTTClient) error {
+	var cfg config.AppConfig
+	var err error
+	if cfg, err = config.LoadConfig(device.Name()); err != nil {
+		return err
+	}
+	if err := Unpublish(device, client); err != nil {
+		return err
+	}
+	if err := cfg.UnRegister(device.Name()); err != nil {
+		return err
+	}
+	return nil
 }
 
 func PublishConfigs(device MQTTDevice, client MQTTClient) error {
