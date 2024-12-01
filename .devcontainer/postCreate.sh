2a410644eb7beb083ff4646dae07e92a1fcc8183
Joshua Rich <joshua.rich@gmail.com>
2024-12-01 15:01:10 +1000

    build(devcontainer): :bug: update dev container configuration for recent podman/vscode changes

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2023 Joshua Rich <joshua.rich@gmail.com>
-#
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-# Stop on errors
 set -e
 
 # Install and configure starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y || exit -1
 mkdir -p ~/.config/fish
-echo "starship init fish | source" >> ~/.config/fish/config.fish
+echo "starship init fish | source" >>~/.config/fish/config.fish
 exit 0
