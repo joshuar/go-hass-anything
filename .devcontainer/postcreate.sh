80c4f9b9a05ca12ca35f12e225dc5d4ba5acf2a9
Joshua Rich <joshua.rich@gmail.com>
2026-02-15 03:36:31 +0000

    build(devcontainer): :arrow_up: update devcontainer

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -4,17 +4,7 @@
 
 set -x
 
-# Add starship to fish shell.
-mkdir -p ~/.config/fish
-echo "starship init fish | source" >>~/.config/fish/config.fish
-
-# Add starship to bash shell.
-echo 'eval "$(starship init bash)"' >>~/.bashrc
-
 cd /workspace
-
-# Update JS packages with bun.
-bun update || exit -1
 
 # Install Go tools.
 go mod tidy
