bb1e691fbe3a8d36e68cb56dc63309abff6e0b32
Joshua Rich <joshua.rich@gmail.com>
2026-09-05 23:02:24 +0000

    build(devcontainer): ⬆️ update devcontainer environment
    - update go and alpine base images
    - update go version
    - improved postcreate script

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -6,10 +6,29 @@ set -x
 
 cd /workspace
 
-# Install Go tools.
-go mod tidy
-go install github.com/air-verse/air@latest
-go install github.com/a-h/templ/cmd/templ@latest
-go install github.com/sigstore/cosign/v3/cmd/cosign@latest
+
+# Install Go packages.
+echo 'set --export PATH "$HOME/go/bin" /go/bin /usr/local/go/bin $PATH' >> ~/.config/fish/config.fish
+echo 'export PATH="$HOME/go/bin:/go/bin:/usr/local/go/bin:$PATH' >> ~/.bashrc
+if [[ -e go.mod ]]; then
+    export PATH="$HOME/go/bin:/go/bin:/usr/local/go/bin:$PATH" && \
+        go mod tidy && \
+        go install golang.org/x/tools/gopls@v0.23.0 && \
+        go install github.com/air-verse/air@v1.67.4 && \
+        go install github.com/magefile/mage@v1.17.2 && \
+        curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/refs/tags/v2.13.2/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.13.2
+fi
+
+if [[ -e ./.custom-gcl.yaml ]]; then
+    golangci-lint custom && \
+    mv /tmp/golangci-lint-v2 $(go env GOPATH)/bin/
+fi
+
+# Install oh-my-posh.
+mkdir -p ~/.local/bin && curl -s https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/tags/v31.1.2/website/static/install.sh | bash -s
+# Set up shells to use oh-my-posh.
+mkdir -p ~/.config/fish \
+    && echo "~/.local/bin/oh-my-posh init fish | source" >>~/.config/fish/config.fish \
+    && echo 'eval "$(~/.local/bin/oh-my-posh init bash)""' >>~/.bashrc
 
 exit 0
